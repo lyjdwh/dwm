7fc2f1f2d335309d7a2658406d65f80c9f20d608
lyjdwh <1412511544@qq.com>
2020-04-17 11:04:33 +0800

    patch hide and restore

--- config.h
+++ config.h
@@ -97,6 +97,8 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
     { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
     { MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
+    { MODKEY,                       XK_k,      hidewin,        {0} },
+    { MODKEY|ShiftMask,             XK_k,      restorewin,     {0} },
     TAGKEYS(                        XK_1,                      0)
     TAGKEYS(                        XK_2,                      1)
     TAGKEYS(                        XK_3,                      2)
