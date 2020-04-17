c6640dd644bd40f623863332a965d3276c6932b2
lyjdwh <1412511544@qq.com>
2020-04-17 11:52:51 +0800

    patch rotate stack
    Stack rotation moves a client from the bottom to the top of the stack

--- config.h
+++ config.h
@@ -99,6 +99,8 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
     { MODKEY,                       XK_k,      hidewin,        {0} },
     { MODKEY|ShiftMask,             XK_k,      restorewin,     {0} },
+    { MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
     TAGKEYS(                        XK_1,                      0)
     TAGKEYS(                        XK_2,                      1)
     TAGKEYS(                        XK_3,                      2)
