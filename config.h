930af18443cf4744889d6fcab1e9e1bc9e1aec89
lyjdwh <1412511544@qq.com>
2020-04-17 12:05:46 +0800

    patch scratchpad
    The scratchpad patch allows you to spawn or restore a floating terminal window. It is usually useful to have one to do some short typing.

--- config.h
+++ config.h
@@ -66,6 +66,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[] = {"alacritty", NULL};
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x24", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
@@ -101,6 +103,7 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_k,      restorewin,     {0} },
     { MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
     { MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
+    { MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
     TAGKEYS(                        XK_1,                      0)
     TAGKEYS(                        XK_2,                      1)
     TAGKEYS(                        XK_3,                      2)
