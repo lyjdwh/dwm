8fb34c1927971f21d05f675c1dd6fa83b1d2169c
lyjdwh <1412511544@qq.com>
2020-10-23 14:01:51 +0800

    modify setlayout keybinding

--- config.h
+++ config.h
@@ -122,7 +122,7 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_l,      tagtoright,     {0} },
     { MODKEY|ControlMask,           XK_t,      setlayout,      {.v = &layouts[0]} },
     { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-    { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+    { MODKEY|ControlMask,           XK_m,      setlayout,      {.v = &layouts[2]} },
     // { MODKEY,                       XK_space,  setlayout,      {0} },
     { MODKEY|ControlMask,           XK_space,  togglefloating, {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
