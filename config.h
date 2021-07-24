42e59092ae94ce6a5976b1352962e9e59d253e59
lyjdwh <1412511544@qq.com>
2021-07-25 01:12:33 +0800

    fix swallow not work

--- config.h
+++ config.h
@@ -38,10 +38,10 @@ static const Rule rules[] = {
      *  WM_CLASS(STRING) = instance, class
      *  WM_NAME(STRING) = title
      */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-  { "Wine",    NULL,     "微信",          1 << 8,    0,          0,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+    /* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+    { "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+    { "Wine",    NULL,     "微信",          1 << 8,    0,          0,           0,        -1 },
+    { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
