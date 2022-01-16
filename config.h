1ecd8ba2550cf63927924177fb72c075fff15105
lyjdwh <1412511544@qq.com>
2022-01-17 00:32:49 +0800

    add more rules

--- config.h
+++ config.h
@@ -39,9 +39,11 @@ static const Rule rules[] = {
      *  WM_NAME(STRING) = title
      */
     /* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-    { "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+    { "st-256color",      NULL,     NULL,           0,         0,          1,           1,        -1 },
     { "Wine",    NULL,     "微信",          1 << 8,    0,          0,           0,        -1 },
+    { "Wine",    NULL,     "企业微信",      1 << 8,    0,          0,           0,        -1 },
     { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+    { "Dragon-drag-and-drop", NULL, "dragon",       0,         1,          0,           1,        -1 },
 };
 
 /* layout(s) */
